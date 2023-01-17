a311981b8597d5f315bbd764fd70f1e68000999b
Anbu Chelvan <114758375+DevAnbu2k4@users.noreply.github.com>
2023-01-17 15:55:05 +0530

    Update settings.py

--- watch/settings.py
+++ watch/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 
