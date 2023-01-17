a53f7f96dce0ec77b5ea4cea4642eb8fcc084a44
Anbu Chelvan <114758375+DevAnbu2k4@users.noreply.github.com>
2023-01-17 17:08:32 +0530

    Update settings.py

--- watch/settings.py
+++ watch/settings.py
@@ -129,9 +129,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 <<<<<<< HEAD
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 =======
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
