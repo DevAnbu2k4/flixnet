3cda459d5d9b0b03a8708d06439fbabe325eed94
Anbu Chelvan <114758375+DevAnbu2k4@users.noreply.github.com>
2023-01-18 16:29:54 +0530

    Update settings.py

--- watch/settings.py
+++ watch/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static/'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static/')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
+
 
 
 # Default primary key field type
