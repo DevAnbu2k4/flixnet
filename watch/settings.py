836ca673dc4a70dad2699bf3914fd406d8340214
DevAnbu2k4 <devanbu2k4@gmail.com>
2023-01-18 16:35:18 +0530

    Static

--- watch/settings.py
+++ watch/settings.py
@@ -128,8 +128,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static/')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static/'),
+
 
 
 # Default primary key field type
