f867f18c23653e6b70871664aed3522257fa1c1f
DevAnbu2k4 <devanbu2k4@gmail.com>
2023-01-18 16:10:07 +0530

    T

--- watch/settings.py
+++ watch/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static'),
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
